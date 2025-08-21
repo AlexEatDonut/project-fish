140efd75273f23df2c88a35acd441cb8ae53cd09
AlexEatDonut <alexeatdonut@gmail.com>
2025-08-21 12:54:35 +0200

    project start

--- maps/devmap_2.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-func _ready() -> void:
-	Playerinfo.playerLocation = Playerinfo.locations.keys()[1]
-	Playerinfo.playerLocationNumber = Playerinfo.locations.DEVMAP
-	print(var_to_str(Playerinfo.playerLocation))
