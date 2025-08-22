be4350a0338f7c3c046e12605158ff66eeb19794
AlexEatDonut <alexeatdonut@gmail.com>
2025-08-22 16:47:09 +0200

    player entity and code edits

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,2 +1,19 @@
 class_name Player
 extends CharacterBody3D
+
+var QueuedFish = []
+
+@onready var state_machine: StateMachine = $StateMachine
+
+@onready var fishing_hud: Control = $CanvasLayer/FishingHud
+
+func _ready() -> void:
+	fishing_hud.StartFishing.connect(start_fishing)
+	match Playerinfo.playerLocation:
+		"DEVMAP":
+			pass
+		"RIVER":
+			pass
+
+func start_fishing():
+	print("start fishing, gal")
