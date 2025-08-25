aac8d1db26d574caed0eb1441e788bedfe86d9d0
AlexEatDonut <alexeatdonut@gmail.com>
2025-08-25 16:00:27 +0200

    added working posterization effect

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,17 +3,29 @@ extends CharacterBody3D
 
 var QueuedFish = []
 
+
+@onready var sub_viewport: SubViewport = $CamPivot/CamLocation/BaseCamera/SubViewportContainer/SubViewport
+@onready var cam_pivot: Node3D = $CamPivot
+
+
 @onready var state_machine: StateMachine = $StateMachine
 
 @onready var fishing_hud: Control = $CanvasLayer/FishingHud
 
+func resize():
+	sub_viewport.size = DisplayServer.window_get_size()
+
 func _ready() -> void:
 	fishing_hud.StartFishing.connect(start_fishing)
+	resize()
 	match Playerinfo.playerLocation:
 		"DEVMAP":
 			pass
 		"RIVER":
 			pass
+
+func _process(delta: float) -> void:
+	resize()
 
 func start_fishing():
 	print("start fishing, gal")
