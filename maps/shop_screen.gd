750b1e680b05ce11db8067fc2d03a00adecd7612
AlexEatDonut <alexeatdonut@gmail.com>
2025-10-16 18:11:32 +0200

    added sounds and changed item data

--- maps/shop_screen.gd
+++ maps/shop_screen.gd
@@ -7,6 +7,9 @@ extends Node3D
 
 @onready var money_count: Label = $CanvasLayer/ShopScreen/bgPanel/MarginsPanel/VBoxContainer/PanelContainer/MoneyContainer/VBoxContainer/MoneyCount
 
+# TODO : figure out how to add some god damn scrolling
+#@onready var grid_baits: GridContainer = $CanvasLayer/ShopScreen/bgPanel/MarginsPanel/VBoxContainer/HBoxContainer/TabContainer/Baits/PanelContainer/MarginContainer/ScrollContainer/GridBaits
+
 @onready var grid_baits: GridContainer = $CanvasLayer/ShopScreen/bgPanel/MarginsPanel/VBoxContainer/HBoxContainer/TabContainer/Baits/PanelContainer/MarginContainer/GridBaits
 @onready var grid_misc: GridContainer = $CanvasLayer/ShopScreen/bgPanel/MarginsPanel/VBoxContainer/HBoxContainer/TabContainer/Misc/PanelContainer/MarginContainer/GridMisc
 
@@ -14,12 +17,25 @@ extends Node3D
 @export var shop_item : PackedScene
 
 @export var bg_curio_music : AudioStreamWAV
+@export var sfx_item_bought : AudioStreamWAV
+@export var sfx_item_hover : AudioStreamWAV
+@export var sfx_money_change: AudioStreamWAV
+@export var sfx_change_category: AudioStreamWAV
 
 #region Hovered item nodes
 @onready var hovered_item_texture: TextureRect = $CanvasLayer/ShopScreen/bgPanel/MarginsPanel/VBoxContainer/HBoxContainer/HoverPreview/MarginContainer/PanelContainer/VBoxContainer/HoveredItemTexture
 @onready var hovered_item_name: Label = $CanvasLayer/ShopScreen/bgPanel/MarginsPanel/VBoxContainer/HBoxContainer/HoverPreview/MarginContainer/PanelContainer/VBoxContainer/HoveredItemName
 @onready var hovered_item_desc: Label = $CanvasLayer/ShopScreen/bgPanel/MarginsPanel/VBoxContainer/HBoxContainer/HoverPreview/MarginContainer/PanelContainer/VBoxContainer/HoveredItemDesc
 @onready var hovered_item_price: Label = $CanvasLayer/ShopScreen/bgPanel/MarginsPanel/VBoxContainer/HBoxContainer/HoverPreview/MarginContainer/PanelContainer/VBoxContainer/HBoxContainer/HoveredItemPrice
+
+#endregion
+
+#region list of all icons used in shop
+## FRESHWATER FISHES - COMMON
+@export var spr_bait_generic = Texture2D
+#111
+@export var spr_ticket_generic = Texture2D
+#211
 
 
 #endregion
@@ -30,50 +46,63 @@ var dev_prices = [300,3000,3000,10000]
 
 var shop_data : Array = [
 	{
-		"icon_path": "res://materials/ui/menu_buttons/bait-icon.svg",
-		"item_name":"Test : Bait Upgrade 1",
-		"item_description":"Bait Upgrade 1 :This is a test description for the hover function. Info about the bait upgrade would be here.",
-		"item_cost":"100 G",
-		"category":"bait"
+		"item_id":1,
+		"icon_id": 111,
+		"item_name":"Rare Bait 
+		Upgrade",
+		"item_description":"A unique blend of spices bundled into one bait. Allows you to catch rare fishes",
+		"item_cost": 300,
+		"category":"bait",
+		"tiered_upgrade":false,
+		"requirements" : 0
 	},
 	{
-		"icon_path": "res://materials/ui/menu_buttons/bait-icon.svg",
-		"item_name":"Test : Bait Upgrade 2",
-		"item_description":"Bait Upgrade 2 : This is a test description for the hover function. Info about the bait upgrade would be here.",
-		"item_cost":"100 G",
-		"category":"bait"
+		"item_id" : 2,
+		"icon_id": 111,
+		"item_name":"Unusual Bait Upgrade",
+		"item_description":"A weird odor comes from this bait, it's unpleasant yet appealing at the same time. Allows you to catch unusual fishes.",
+		"item_cost": 1700,
+		"category":"bait",
+		"tiered_upgrade":true,
+		"requirements" : 1
 	},
 	{
-		"icon_path": "res://materials/ui/menu_buttons/bait-icon.svg",
-		"item_name":"Test : Bait Upgrade 3",
-		"item_description":"Bait Upgrade 3 :This is a test description for the hover function. Info about the bait upgrade would be here.",
-		"item_cost":"100 G",
-		"category":"bait"
+		"item_id": 3,
+		"icon_id": 111,
+		"item_name":"Legendary Bait Upgrade",
+		"item_description":"An odorless bait that leaves you wandering if you got scammed. It looks mundane, but you cannot decifer what is in there. Allows you to catch legendary fishes.",
+		"item_cost": 6400,
+		"category":"bait",
+		"tiered_upgrade":true,
+		"requirements" : 2
 	},
 	{
-		"icon_path": "res://materials/ui/menu_buttons/bait-icon.svg",
-		"item_name":"Test : Bait Upgrade 4",
-		"item_description":"Bait Upgrade 4 : This is a test description for the hover function. Info about the bait upgrade would be here.",
-		"item_cost":"100 G",
-		"category":"bait"
-	},
-	{
-		"icon_path": "res://materials/ui/menu_buttons/bait-icon.svg",
-		"item_name":"Test : Bait Upgrade 5",
-		"item_description":"Bait Upgrade 5 : This is a test description for the hover function. Info about the bait upgrade would be here.",
-		"item_cost":"100 G",
-		"category":"bait"
-	},
-	{
-		"icon_path": "res://materials/ui/menu_buttons/ticket-fill.svg",
-		"item_name":"Test : Ticket",
-		"item_description":"This is a test description for the hover function. Information about the ticket would be here.",
-		"item_cost":"300 G",
-		"category":"misc"
+		"item_id": 4,
+		"icon_id": 211,
+		"item_name":"Ticket : Lakeside",
+		"item_description":"A reusable traveling ticket to the lakeside. Allows access to the lakeside from the Travel menu",
+		"item_cost": 3000,
+		"category":"misc",
+		"tiered_upgrade" : false,
+		"requirements" : 0
 	}
 ]
 
 var shop_item_id : int = 0
+
+func give_item_icon(SpriteRect, spritenumber)->void :
+	match spritenumber:
+		## BAITS
+		111:
+			SpriteRect.texture = spr_bait_generic
+		## TICKETS 
+		211:
+			SpriteRect.texture = spr_ticket_generic
+		_:
+			SpriteRect.texture = null
+
+
+
 
 func _ready() -> void:
 	setup_shop()
@@ -87,6 +116,7 @@ func lock_button(target_button):
 
 func update_money():
 	money_count.text = str(Playerinfo.money)
+	#SoundManager.play_ui_sound(sfx_money_change)
 
 #TODO : Change this yandere dev type shit asap. This is misarable. Actually putting me to tears of sadness. This shit outta explode.
 #func check_all_items_availbability():
@@ -114,17 +144,20 @@ func setup_shop() -> void:
 				grid_misc.add_child(temp)
 		#grid.add_child(temp)
 		temp.setup(data, shop_item_id)
+		give_item_icon(temp.item_icon, temp.get_icon_id(data) )
 		shop_item_id += 1
 
 func on_item_buy_pressed(id : int) -> void:
-	#print(shop_data[id].get("item_name_label")+ " bought.")
 	print(shop_data[id]["item_name"])
+	Playerinfo.buy_upgrade(int(shop_data[id]["item_id"]))
+	SoundManager.play_ui_sound(sfx_item_bought)
 
 func on_item_hovered(id : int):
-	#hovered_item_texture.texture = shop_data[id]["icon_path"]
+	SoundManager.play_ui_sound(sfx_item_hover)
+	#hovered_item_texture.texture = shop_data[id]["icon_id"]
 	hovered_item_name.text = shop_data[id]["item_name"]
 	hovered_item_desc.text = shop_data[id]["item_description"]
-	hovered_item_price.text = shop_data[id]["item_cost"]
+	hovered_item_price.text = str(shop_data[id]["item_cost"])
 	
 #func _on_lake_ticket_btn_pressed() -> void:
 	#Playerinfo.buy_ticket_1()
@@ -151,7 +184,7 @@ func on_item_hovered(id : int):
 	#check_all_items_availbability()
 
 func _on_return_btn_pressed() -> void:
-	SoundManager.stop_music(1  )
+	SoundManager.stop_music(1)
 	var map_id = Playerinfo.playerLocationNumber
 	print(map_id)
 	match(map_id):
@@ -163,3 +196,7 @@ func _on_return_btn_pressed() -> void:
 			get_tree().change_scene_to_file("res://maps/fishingspot_lake.tscn")
 		_:
 			get_tree().change_scene_to_file("res://maps/fishingspot_river.tscn")
+
+
+func _on_tab_container_tab_changed(tab: int) -> void:
+	SoundManager.play_ui_sound(sfx_change_category)
