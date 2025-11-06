99d9b0d5e3609f234d1154dca1c51bda357e55a5
AlexEatDonut <alexeatdonut@gmail.com>
2025-11-06 18:40:47 +0100

    moved fishing rod data into file

--- scripts/playerinfo.gd
+++ scripts/playerinfo.gd
@@ -2,69 +2,17 @@ extends Node
 
 var can_repair_rod : bool = false
 
-var fishing_rods :Dictionary ={
-		"300" : {
-		"item_id": 300,
-		"icon_id": 211,
-		"item_name":"Wooden Rod",
-		"item_description":"Your default rod ! Not very good, but can still get work done.",
-		"strength_desc" : "Clearly you can get better.",
-		"weakness-desc" : "It has nothing going for it.",
-		"base_durability" : 50,
-		"baseline_repair_cost" : 10,
-		"durability_repair_multiplier" : 1.5,
-		"lurk_time_multiplier" : 1,
-		"catch_window_multiplier" : 1,
-		"fish_weights_multiplier" : [1,1,1,1,1]
-	},
-	"301" : {
-		"item_id": 301,
-		"icon_id": 211,
-		"item_name":"Fishin' Fun branded rod",
-		"item_description":"Your run of the mill store rod. Better than a stick of wood, that's for sure.",
-		"strength_desc" : "Better than a piece of wood.",
-		"weakness-desc" : "It has nothing else going for it..",
-		"base_durability" : 120,
-		"baseline_repair_cost" : 10,
-		"durability_repair_multiplier" : 2,
-		"lurk_time_multiplier" : 1,
-		"catch_window_multiplier" : 1,
-		"fish_weights_multiplier" : [1,1,1,1,1]
-	},
-	"302" : {
-		"item_id": 302,
-		"icon_id": 211,
-		"item_name":"'Pro' fisher branded rod",
-		"item_description":"A pricier rod equipped with a bait enhancer.",
-		"strength_desc" : "Reduced fish lurk time (- 10%).",
-		"weakness-desc" : "Less durability, costier durability repair.",
-		"base_durability" : 70,
-		"baseline_repair_cost" : 12,
-		"durability_repair_multiplier" : 5,
-		"lurk_time_multiplier" : 1,
-		"catch_window_multiplier" : 1,
-		"fish_weights_multiplier" : [1,1,1,1,1]
-	},
-	"303" :{
-		"item_id": 303,
-		"icon_id": 211,
-		"item_name":"Enticing rod",
-		"item_description":"A rod that makes fish want to stay in its bait.",
-		"strength_desc" : "Extended catch window (+10%).",
-		"weakness-desc" : "+5% lurk time duration, costier durability repair.",
-		"base_durability" : 100,
-		"baseline_repair_cost" : 10,
-		"durability_repair_multiplier" : 4,
-		"lurk_time_multiplier" : 1.05,
-		"catch_window_multiplier" : 1,
-		"fish_weights_multiplier" : [1,1,1,1,1]
-	}
-}
+var rodsDataFile = "res://scripts/rods_data.json"
+var json_as_text = FileAccess.get_file_as_string(rodsDataFile)
+var fishing_rods = JSON.parse_string(json_as_text)
 
-var r300_durability : float
-var r301_durability : float
-var r302_durability : float
-var r303_durability : float
+
+var all_rods_duability = []
+
+#var r300_durability : float
+#var r301_durability : float
+#var r302_durability : float
+#var r303_durability : float
 
 @export var DEFAULT_ROD_REPAIR_COST : float = 10
 var RepairCost = DEFAULT_ROD_REPAIR_COST
@@ -88,7 +36,7 @@ var maximum_catching_delay  = DEFAULT_MAXIMUM_CATCHING_DELAY
 
 var debug_mode : bool = false
 
-var equipped_rod : int = 301
+var equipped_rod : int = 300
 var equipped_rod_data : Dictionary
 
 var rod_bait_value = 0
@@ -125,19 +73,6 @@ var start_fishing : bool = false
 var in_fishing_position : bool = false
 
 var playerIsBroke : bool = false
-
-#region Dev Shop : Activating certain vars for testing
-#TODO : technical debt : make it not suck as hard
-var ticket_ownership_1 : bool = false
-var bait_upgrade_ownership_1 : bool = false
-var bait_upgrade_ownership_2 : bool = false
-var bait_upgrade_ownership_3 : bool = false
-
-var bait_upgrade_rare_owned : bool = false
-var bait_upgrade_unusual_owned : bool = false
-var bait_upgrade_legendary_owned : bool = false
-
-#endregion
 
 #region Rod Durability
 var rod_durability = max_rod_durability:
@@ -208,16 +143,22 @@ func get_rod_data():
 	match equipped_rod:
 		300:
 			equipped_rod_data = fishing_rods["300"]
-			print(equipped_rod_data["strength_desc"])
 		301:
 			equipped_rod_data = fishing_rods["301"]
-			print(equipped_rod_data["strength_desc"])
 		302:
 			equipped_rod_data = fishing_rods["302"]
-			print(equipped_rod_data["strength_desc"])
 		303:
 			equipped_rod_data = fishing_rods["303"]
-			print(equipped_rod_data["strength_desc"])
+	#print(equipped_rod_data["strength_desc"])
+
+func get_fish_lurk_timer():
+	var rng_lurk_timer = RandomNumberGenerator.new()
+	var lurk_timer = rng_lurk_timer.randf_range(queued_fish["lurk_time"][0], queued_fish["lurk_time"][1])
+	return(snappedf(lurk_timer * equipped_rod_data["lurk_time_multiplier"] ,0.01))
+
+	
+func get_fish_catch_window():
+	return(queued_fish["catch_time"] * equipped_rod_data["catch_window_multiplier"])
 
 #endregion
 #region Money variable
@@ -261,18 +202,11 @@ func increase_money(moneyGiven, ratio : float = 1):
 		print("Error ! moneyGiven was 0 or lower. Not multiplying to avoid calculus issues.")
 #endregion
 
-func is_lake_unlocked():
-	if ticket_ownership_1 == true:
-		return true
-	else:
-		return false
 
 func buy_upgrade(id : int):
 	owned_upgrades.append(id)
 	print(owned_upgrades)
 
-func buy_ticket_1():
-	ticket_ownership_1 = true
 
 func check_bait_value():
 	if owned_upgrades.has(101):
