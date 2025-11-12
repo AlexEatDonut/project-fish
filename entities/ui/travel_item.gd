709ed8f5bc72c90288a21684dca5d69f3d1d7c18
AlexEatDonut <alexeatdonut@gmail.com>
2025-11-12 12:59:15 +0100

    added more filters for the travel menu

--- entities/ui/travel_item.gd
+++ entities/ui/travel_item.gd
@@ -6,13 +6,23 @@ signal item_hovered(id)
 
 @onready var location_icon: TextureRect = $MarginContainer/VBoxContainer/MaxSizeContainer/PanelContainer2/TextureRect
 
+var location_thumbs = ["res://materials/ui/travel_locations/location_river_thumb.png","res://materials/ui/travel_locations/location_lake_thumb.png"]
+
 @onready var location_name_label: Label = $MarginContainer/VBoxContainer/MaxSizeContainer2/PanelContainer/MarginContainer/VBoxContainer/LocationNameLabel
 
 @onready var travel_item_btn: Button = $TravelItemBtn
 
-var location_id : int
+@onready var blur_panel: PanelContainer = $MarginContainer/BlurPanel
+@onready var here_label: Label = $MarginContainer/BlurPanel/CenterContainer/HereLabel
+@onready var locked_label: Label = $MarginContainer/BlurPanel/CenterContainer/LockedLabel
+
+
+var location_item_id : int
+var location_name : String
 
 var upgrade_tier = 0
+
+var is_locked : bool = false
 
 var id : int
 
@@ -20,25 +30,67 @@ func setup(data: Dictionary, p_id : int) -> void:
 	#item_icon.texture = load(data.get("icon_path"))
 	#item_icon.texture = load(data.get("res://materials/ui/menu_buttons/bait-icon.svg"))
 	location_name_label.text = data["location_name"]
-	location_id = int(data["location_id"])
+	location_item_id = int(data["item_id"])
+	location_name = data["location_name"]
 	id = p_id
+	change_icon(data["location_id"])
+	
+	check_item_equipped(data["location_name"])
+	check_item_owned(data["location_id"])
+	blur_reset()
 
 func _process(delta: float) -> void:
-	is_item_clickable()
+	is_item_clickable(location_item_id, location_name )
 
-func is_item_clickable():
-	match check_item_not_owned(location_id):
+func is_item_clickable(locationitemid : int, locationname : String):
+	match check_item_owned(locationitemid):
 		true:
-			travel_item_btn.disabled = false
+			pass
 		false :
-			travel_item_btn.disabled = true
+			blur_locked_enable()
+	match check_item_equipped(locationname):
+		true:
+			blur_youarehere_enable()
+		false:
+			pass
 
-func check_item_not_owned(locationid : int):
-	#if the player DOESN'T own the targetted object, it is not owned and therefore is true
-	if !Playerinfo.owned_upgrades.has(locationid):
+func change_icon(locationid : int):
+	var correct_array_id = locationid - 1
+	location_icon.texture = load(location_thumbs[correct_array_id])
+
+func check_item_owned(locationid : int):
+	#if the player owns the targetted object, it is owned and therefore is true
+	if Playerinfo.owned_upgrades.has(locationid) or locationid == 200:
 		return(true)
 	else:
 		return(false)
+
+func check_item_equipped(locationname):
+	if Playerinfo.playerLocation == locationname:
+		return(true)
+	else:
+		return(false)
+
+func blur_youarehere_enable() -> void :
+	blur_panel.visible = true
+	here_label.visible = true
+	travel_item_btn.disabled = true
+	is_locked = true
+
+func blur_locked_enable() -> void :
+	blur_panel.visible = true
+	locked_label.visible = true
+	travel_item_btn.disabled = true
+	is_locked = true
+
+func blur_reset() -> void:
+	blur_panel.visible = false
+	here_label.visible = false
+	locked_label.visible = false
+	travel_item_btn.disabled = false
+	is_locked = false
+
+
 
 func _on_travel_item_btn_pressed() -> void:
 	emit_signal("travel_destination_pressed", id)
