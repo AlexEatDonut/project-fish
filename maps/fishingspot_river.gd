9c0b609e8bcce891e3bddd734f55a3723d183e9f
AlexEatDonut <alexeatdonut@gmail.com>
2025-11-28 12:55:33 +0100

    edited and integrated ambiance track

--- maps/fishingspot_river.gd
+++ maps/fishingspot_river.gd
@@ -1,6 +1,11 @@
 extends Node3D
 
+@export var ambience1 : AudioStreamWAV
+
 func _ready() -> void:
 	Playerinfo.playerLocation = Playerinfo.locations.keys()[1]
 	Playerinfo.playerLocationNumber = Playerinfo.locations.RIVER
-	print(var_to_str(Playerinfo.playerLocation))
+	#print(var_to_str(Playerinfo.playerLocation))
+	SoundManager.play_ambient_sound(ambience1)
+func _exit_tree() -> void:
+	SoundManager.ambient_sounds.stop_all(0.25)
