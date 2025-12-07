c306c9dd229c2672160e807fb387cacbca43c33c
AlexEatDonut <alexeatdonut@gmail.com>
2025-12-07 14:13:33 +0100

    fixed ambience not playing in lake

--- maps/fishingspot_lake.gd
+++ maps/fishingspot_lake.gd
@@ -1,12 +1,13 @@
 extends Node3D
 
-@export var ambience1 : AudioStreamWAV
+@export var ambience2 : AudioStreamWAV
 
 func _ready() -> void:
 	Playerinfo.playerLocation = Playerinfo.locations.keys()[2]
 	Playerinfo.playerLocationNumber = Playerinfo.locations.LAKE
 	#print(var_to_str(Playerinfo.playerLocation))
-	SoundManager.play_ambient_sound(ambience1)
+	await get_tree().create_timer(0.05).timeout
+	SoundManager.play_ambient_sound(ambience2)
 
 func _exit_tree() -> void:
-	SoundManager.ambient_sounds.stop_all(0.25)
+	SoundManager.ambient_sounds.stop_all()
--- maps/fishingspot_river.gd
+++ maps/fishingspot_river.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	Playerinfo.playerLocation = Playerinfo.locations.keys()[1]
 	Playerinfo.playerLocationNumber = Playerinfo.locations.RIVER
 	#print(var_to_str(Playerinfo.playerLocation))
+	await get_tree().create_timer(0.05).timeout
 	SoundManager.play_ambient_sound(ambience1)
 func _exit_tree() -> void:
-	SoundManager.ambient_sounds.stop_all(0.25)
+	SoundManager.ambient_sounds.stop_all()
